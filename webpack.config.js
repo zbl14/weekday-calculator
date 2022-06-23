f891d8fa2991592c50045ce83689c652524b8602
zbl14 <ifthereisoneday@gmail.com>
2022-06-23 14:35:48 -0700

    update title to weekday-calculator

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: ' ',
+      title: 'Day of week',
       template: './src/index.html',
       inject: 'body'
     })
