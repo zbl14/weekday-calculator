367595ba5f8217b908353e401e149332aa076f50
zbl14 <ifthereisoneday@gmail.com>
2022-06-23 10:26:05 -0700

    import Date

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+import { Date } from '../src/js/date.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
+
+$(document).ready(function() {
+let date = new Date()
+
+});
